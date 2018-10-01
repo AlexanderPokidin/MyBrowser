2bdf0c23c3fa649cacc94f4f527b13cb087e02c1
AlexanderPokidin <a.pokidin@gmail.com>
2018-10-01 12:06:53 +0300

    Test WebView

--- app/src/main/java/com/pokidin/a/mybrowser/MainActivity.java
+++ app/src/main/java/com/pokidin/a/mybrowser/MainActivity.java
@@ -1,8 +1,12 @@
 package com.pokidin.a.mybrowser;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mWebView = findViewById(R.id.webView);
+//        mWebView.getSettings().setJavaScriptEnabled(true);
+
+        mWebView.setWebViewClient(new MyWebViewClient());
         mWebView.loadUrl("https://developer.android.com");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
     }
 }
